5aa70eebbfa0fa3a754fea2ffdbdb343e7c686bd
Alvin <aat8641@mavs.uta.edu>
2025-04-09 21:42:39 -0500

    created js file for homescreen

---
+++ frontend/homescreen.js
@@ -0,0 +1,8 @@
+var info = {
+    'ding tea': {'wintermelon milk tea': {rating: 5, review: 'good'}, 'peach oolong tea': {rating: 4, review: 'good'}},
+    'boba tea': {'mango milk tea': {rating: 5, review: 'good'}}
+  };
+
+
+
+function 
