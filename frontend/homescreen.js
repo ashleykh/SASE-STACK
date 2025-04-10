7f289a57131c31c2735219554a0af969a1adea94
Peter-Phi-Tran <ptran.tech@outlook.com>
2025-04-09 22:17:18 -0500

    jaja:

--- frontend/homescreen.js
+++ frontend/homescreen.js
@@ -2,7 +2,3 @@ var info = {
     'ding tea': {'wintermelon milk tea': {rating: 5, review: 'good'}, 'peach oolong tea': {rating: 4, review: 'good'}},
     'boba tea': {'mango milk tea': {rating: 5, review: 'good'}}
   };
-
-
-
-function 
